bb5b3f7530c667ea1b6c71b48f8181d09bea2f90
Ilya12184 <volostnov.il@yandex.ru>
2023-01-31 12:44:44 +0300

    Add files

---
+++ bot.py
@@ -0,0 +1,25 @@
+import time
+import telebot
+
+bot = telebot.TeleBot('6102482906:AAFzv89CWk55lYUY7YcNd1Kx3L7OW8IaGng')
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    mess = f'Привет,<b>{message.from_user.full_name}</b>'
+    bot.send_message(message.chat.id, mess, parse_mode='html')
+
+
+    for i in range(20):
+        with open('flowers.jpg', 'rb') as f:
+            bot.send_message(message.chat.id, 'Вам букетик!', parse_mode='html')
+            bot.send_photo(message.chat.id, f)
+            time.sleep(60*30)
+
+# @bot.message_handler(content_types=['text'])
+# def choise(poz_neg):
+#     if poz_neg.text == 'Нет':
+#         bot.send_message(poz_neg.chat.id, 'Ужас!')
+#     elif poz_neg.text == 'Да':
+#         bot.send_message(poz_neg.chat.id, 'Красавчик, продолжай в том же духе!')
+
+bot.polling(none_stop=True)
